8106b60ffdbcd983dd1482defac7db72bd6663d0
Seng Wee <sengwee_new@hotmail.com>
2019-11-01 17:33:11 +0800

    refactor and adding deployment resource limits

--- deploy.sh
+++ deploy.sh
@@ -33,36 +33,36 @@ az provider register --namespace Microsoft.ContainerService
 
 az group create --name $RESOURCE_GROUP --location southeastasia
 
-az aks create -g $RESOURCE_GROUP -n $KUBE_NAME --node-count 5 --enable-vmss --enable-cluster-autoscaler --min-count 3 --max-count 6 --node-vm-size 'Standard_E2s_v3'
+az aks create -g $RESOURCE_GROUP -n $KUBE_NAME --node-count 5 --enable-vmss --enable-cluster-autoscaler --min-count 5 --max-count 8 --node-vm-size 'Standard_E2s_v3'
 
 az aks get-credentials -g $RESOURCE_GROUP -n $KUBE_NAME
 
-# kubectl create -f pvc/nfs-server-azure-pvc.yml
+kubectl create -f pvc/nfs-server-azure-pvc.yml
  
-# kubectl create -f rc/nfs-server-rc.yml
+kubectl create -f rc/nfs-server-rc.yml
 
-# kubectl create -f services/nfs-server-service.yml 
-# 
-# NFS_IP=$(kubectl get service nfs-server | awk '{print $3}' | sed -n 2p)
-#  
-# sed "s/NFS_CLUSTER_IP/$NFS_IP/g" pv/nfs-pv-template.yml > nfs-pv.yml
-#  
-# kubectl create -f nfs-pv.yml
-#  
-# rm nfs-pv.yml
-# 
-# kubectl create -f pvc/nfs-pvc.yml
-# 
-# kubectl create -f deployment/master-rc.yml
-# 
-# kubectl create -f services/master-svc.yml
-# 
-# MASTER_STATE=$(kubectl get service master-service | grep -i pending)
-# while [[ ! -z $MASTER_STATE ]]
-# do
-#     sleep 10
-#     echo 'waiting for master to init'
-#     MASTER_STATE=$(kubectl get service master-service | grep -i pending)
-# done
-# 
-# kubectl create -f deployment/worker-rc.yml
+kubectl create -f services/nfs-server-service.yml 
+
+NFS_IP=$(kubectl get service nfs-server | awk '{print $3}' | sed -n 2p)
+ 
+sed "s/NFS_CLUSTER_IP/$NFS_IP/g" pv/nfs-pv-template.yml > nfs-pv.yml
+ 
+kubectl create -f nfs-pv.yml
+ 
+rm nfs-pv.yml
+
+kubectl create -f pvc/nfs-pvc.yml
+
+kubectl create -f deployment/master-rc.yml
+
+kubectl create -f services/master-svc.yml
+
+MASTER_STATE=$(kubectl get service master-service | grep -i pending)
+while [[ ! -z $MASTER_STATE ]]
+do
+    sleep 10
+    echo 'waiting for master to init'
+    MASTER_STATE=$(kubectl get service master-service | grep -i pending)
+done
+
+kubectl create -f deployment/worker-rc.yml
