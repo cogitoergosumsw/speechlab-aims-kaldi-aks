58c356313228d5fc4f7c2cef2935279b005410b4
Seng Wee <sengwee_new@hotmail.com>
2020-01-03 01:47:35 +0800

    like before

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ export KUBE_NAME=kaldi-feature-test
 export RESOURCE_GROUP=kaldi-test
 export STORAGE_ACCOUNT_NAME=kalditeststorage
 export LOCATION=southeastasia
-export MODEL_SHARE=model-azurefile-share
+export MODEL_SHARE=online-models
 export NAMESPACE=kaldi-test
 export CONTAINER_REGISTRY=kalditest
 export DOCKER_IMAGE_NAME=kalditestscaled
@@ -35,8 +35,7 @@ az provider show -n Microsoft.ContainerService | grep registrationState
 VMSS_STATE=$(az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService/VMSSPreview')].{Name:name,State:properties.state}" | grep -i registered)
 AKS_LOAD_BALANCER_STATE=$(az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService/AKSAzureStandardLoadBalancer')].{Name:name,State:properties.state}" | grep -i registered)
 
-while [[ -z $VMSS_STATE ]]
-do
+while [[ -z $VMSS_STATE ]]; do
     VMSS_STATE=$(az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService/VMSSPreview')].{Name:name,State:properties.state}" | grep -i registered)
     echo 'Waiting for Microsoft.ContainerService/VMSSPreview registration'
     sleep 3
@@ -45,8 +44,7 @@ do
 done
 echo $VMSS_STATE
 
-while [[ -z $AKS_LOAD_BALANCER_STATE ]]
-do
+while [[ -z $AKS_LOAD_BALANCER_STATE ]]; do
     AKS_LOAD_BALANCER_STATE=$(az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService/AKSAzureStandardLoadBalancer')].{Name:name,State:properties.state}" | grep -i registered)
     echo 'Waiting for Microsoft.ContainerService/AKSAzureStandardLoadBalancer registration'
     sleep 3
@@ -62,7 +60,7 @@ az acr create --name $CONTAINER_REGISTRY --resource-group $RESOURCE_GROUP --sku 
 
 az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP -l $LOCATION --sku Standard_LRS --kind StorageV2
 
-export AZURE_STORAGE_CONNECTION_STRING=`az storage account show-connection-string -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP -o tsv`
+export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-string -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP -o tsv)
 
 # Create the file share
 az storage share create -n $MODEL_SHARE --connection-string $AZURE_STORAGE_CONNECTION_STRING
@@ -78,38 +76,73 @@ az storage container create -n $AZURE_CONTAINER_NAME --account-key $STORAGE_KEY 
 
 # prompt to put the models in the models directory
 NUM_MODELS=$(find ./models/ -maxdepth 1 -type d | wc -l)
-if [ $NUM_MODELS -gt 1 ]
-then
-    az storage blob upload-batch -d $AZURE_CONTAINER_NAME --account-key $STORAGE_KEY --account-name $STORAGE_ACCOUNT_NAME -s models/
+if [ $NUM_MODELS -gt 1 ]; then
+    # az storage blob upload-batch -d $AZURE_CONTAINER_NAME --account-key $STORAGE_KEY --account-name $STORAGE_ACCOUNT_NAME -s models/
+    az storage file upload-batch -d $MODEL_SHARE --account-key $STORAGE_KEY --account-name $STORAGE_ACCOUNT_NAME -s models/
 else
     printf "\n"
     printf "##########################################################################\n"
     echo "Please put at least one model in the ./models directory before continuing"
     printf "##########################################################################\n"
-    
+
     exit 1
 fi
+# echo "$NUM_MODELS models uploaded to Azure Blob storage | Blob Container: $AZURE_CONTAINER_NAME"
+echo "$NUM_MODELS models uploaded to Azure File Share storage | Azure Files: $MODEL_SHARE"
 
-sed "s/AZURE_STORAGE_ACCOUNT_DATUM/$STORAGE_ACCOUNT_NAME/g" docker/secret/run_kubernetes_secret_template.yaml > docker/secret/run_kubernetes_secret.yaml
+sed "s/AZURE_STORAGE_ACCOUNT_DATUM/$STORAGE_ACCOUNT_NAME/g" docker/secret/run_kubernetes_secret_template.yaml >docker/secret/run_kubernetes_secret.yaml
 sed -i "s|AZURE_STORAGE_ACCESS_KEY_DATUM|$STORAGE_KEY|g" docker/secret/run_kubernetes_secret.yaml
 
-sed "s/AZURE_STORAGE_ACCOUNT_DATUM/$STORAGE_ACCOUNT_NAME/g" docker/secret/docker-compose-local_template.env > docker/secret/docker-compose-local.env
+sed "s/AZURE_STORAGE_ACCOUNT_DATUM/$STORAGE_ACCOUNT_NAME/g" docker/secret/docker-compose-local_template.env >docker/secret/docker-compose-local.env
 sed -i "s|AZURE_STORAGE_ACCESS_KEY_DATUM|$STORAGE_KEY|g" docker/secret/docker-compose-local.env
 
 # get docker registry password
 CONTAINER_REGISTRY_PASSWORD=$(az acr credential show -n kalditest --query passwords[0].value | grep -oP '"\K[^"]+')
 echo "Container Registry | username: $CONTAINER_REGISTRY | password: $CONTAINER_REGISTRY_PASSWORD"
 
+# KALDI_AKS_VNET="kaldi-feature-test-vnet"
+
+# az network vnet create \
+#     --name $KALDI_AKS_VNET \
+#     --resource-group $RESOURCE_GROUP \
+#     --subnet-name default
+
+# SUBNET_ID=$(az network vnet show -g kaldi-test -n kaldi-test-vnet --query subnets[0].id --output tsv)
+
+# # create the VM that will be the NFS server
+# az vm create \
+#     --resource-group $RESOURCE_GROUP \
+#     --name nfs-server-vm \
+#     --image UbuntuLTS \
+#     --subnet $SUBNET_ID \
+#     --admin-username kaldiadmin \
+#     --generate-ssh-keys
+
+# NFS_SERVER_IP=$(az vm list-ip-addresses -g kaldi-test -n nfs-server-vm --query [0].virtualMachine.network.publicIpAddresses[0].ipAddress -o tsv)
+# NFS_SERVER_PRIVATE_IP=$(az vm list-ip-addresses -g kaldi-test -n nfs-server-vm --query [0].virtualMachine.network.privateIpAddresses[0] -o tsv)
+# chmod +x nfs/server/nfs-server-setup.sh
+# ssh -t kaldiadmin@$NFS_SERVER_IP 'sudo mkdir /home/kaldiadmin/setup'
+# scp nfs/server/nfs-server-setup.sh kaldiadmin@$NFS_SERVER_IP:/home/kaldiadmin/setup/nfs-server-setup.sh
+# ssh -t kaldiadmin@$NFS_SERVER_IP 'sudo /home/kaldiadmin/setup/nfs-server-setup.sh'
+
+# sed "s/NFS_INTERNAL_IP/$NFS_SERVER_PRIVATE_IP/g" docker/helm/values.yaml.template > docker/helm/kaldi-feature-test/values.yaml
+
 az aks create \
--g $RESOURCE_GROUP \
--n $KUBE_NAME \
---node-count 3 \
---enable-vmss \
---enable-cluster-autoscaler \
---min-count 3 \
---max-count 8 \
---node-vm-size Standard_D4_v3 \
---load-balancer-sku standard
+    -g $RESOURCE_GROUP \
+    -n $KUBE_NAME \
+    --node-count 3 \
+    --enable-vmss \
+    --enable-cluster-autoscaler \
+    --min-count 3 \
+    --max-count 8 \
+    --node-vm-size Standard_D4_v3 \
+    --load-balancer-sku standard
+    # --network-plugin kubenet \
+    # --service-cidr 10.0.0.0/16 \
+    # --dns-service-ip 10.0.0.10 \
+    # --pod-cidr 10.244.0.0/16 \
+    # --vnet-subnet-id $SUBNET_ID \
+    # --docker-bridge-address 172.17.0.1/16
 
 az aks get-credentials -g $RESOURCE_GROUP -n $KUBE_NAME --admin --overwrite-existing
 
@@ -155,20 +188,25 @@ kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller
 
-
 # Create a service account to access private azure docker registry
 ##################################################################
 kubectl create secret docker-registry azure-cr-secret \
---docker-server=https://kalditest.azurecr.io \
---docker-username=$CONTAINER_REGISTRY \
---docker-password=$CONTAINER_REGISTRY_PASSWORD \
---namespace $NAMESPACE
+    --docker-server=https://kalditest.azurecr.io \
+    --docker-username=$CONTAINER_REGISTRY \
+    --docker-password=$CONTAINER_REGISTRY_PASSWORD \
+    --namespace $NAMESPACE
+
+export MODELS_FILESHARE_SECRET="models-files-secret"
+# k8 secret for accessing Azure File share
+kubectl create secret generic $MODELS_FILESHARE_SECRET --from-literal=azurestorageaccountname=$STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGE_KEY
 
 # after filling in the azure storage account details...
 #########################################################
 kubectl apply -f docker/secret/run_kubernetes_secret.yaml
 
-# kubectl create secret generic volume-azurefile-storage-secret --from-literal=azurestorageaccountname=$STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGE_KEY
+# create the persistent volume that will store the models
+kubectl apply -f pv/kaldi-models-pv.yaml
+kubectl apply -f pv/kaldi-models-pvc.yaml
 
 # helm package docker/helm/speechlab/
 
--- docker/scripts/master_server_addon.py
+++ docker/scripts/master_server_addon.py
@@ -1,35 +1,43 @@
 from kubernetes import client, config
 from kubernetes.client.rest import ApiException
-import random, string, os, sys
+import random
+import string
+import os
+import sys
 
 
 import logging
 
 IMAGE = "kalditest.azurecr.io/kalditestscaled"
 AZURE_STORAGE_ACCOUNT = os.getenv("AZURE_STORAGE_ACCOUNT", False)
-AZURE_STORAGE_ACCESS_KEY = os.getenv("AZURE_STORAGE_ACCESS_KEY", False) 
-AZURE_CONTAINER = os.getenv("AZURE_CONTAINER", False)  
+AZURE_STORAGE_ACCESS_KEY = os.getenv("AZURE_STORAGE_ACCESS_KEY", False)
+AZURE_CONTAINER = os.getenv("AZURE_CONTAINER", False)
 MASTER = os.getenv("MASTER", False)
 NAMESPACE = os.getenv("NAMESPACE", False)
+MODELS_FILESHARE_SECRET: os.getenv("MODELS_FILESHARE_SECRET", False)
+MODELS_SHARE_NAME: os.getenv("MODELS_SHARE_NAME", False)
 
-if (NAMESPACE == False or 
-    AZURE_STORAGE_ACCOUNT == False or 
-    AZURE_STORAGE_ACCESS_KEY == False or 
-    AZURE_CONTAINER == False or 
-    MASTER == False or 
-    NAMESPACE == False):
-        sys.exit("No values for NAMESPACE=" \
-        +str(NAMESPACE) \
-        +" AZURE_STORAGE_ACCOUNT=" \
-        +str(AZURE_STORAGE_ACCOUNT) \
-        +" AZURE_STORAGE_ACCESS_KEY=" \
-        +str(AZURE_STORAGE_ACCESS_KEY ) \
-        +" AZURE_CONTAINER=" \
-        +str(AZURE_CONTAINER ) \
-        +" MASTER="+str(MASTER ) \
-        +" NAMESPACE="+str(NAMESPACE ))
+if (NAMESPACE == False or
+    AZURE_STORAGE_ACCOUNT == False or
+    AZURE_STORAGE_ACCESS_KEY == False or
+    AZURE_CONTAINER == False or
+    MASTER == False or
+        NAMESPACE == False):
+    sys.exit("No values for NAMESPACE="
+             + str(NAMESPACE)
+             + " AZURE_STORAGE_ACCOUNT="
+             + str(AZURE_STORAGE_ACCOUNT)
+             + " AZURE_STORAGE_ACCESS_KEY="
+             + str(AZURE_STORAGE_ACCESS_KEY)
+             + " AZURE_CONTAINER="
+             + str(AZURE_CONTAINER)
+             + " MASTER="+str(MASTER)
+             + " NAMESPACE="+str(NAMESPACE)
+             + " MODELS_FILESHARE_SECRET="+str(MODELS_FILESHARE_SECRET)
+             + " MODELS_SHARE_NAME="+str(MODELS_SHARE_NAME))
 
 config.load_kube_config()
+
 
 def spawn_worker(model):
     """
@@ -54,53 +62,74 @@ def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
 def create_job(MODEL):
 
     assert MODEL is not None, "model name is None, cannot spawn a new worker"
-  
+
     api = client.BatchV1Api()
- 
 
     body = client.V1Job(api_version="batch/v1", kind="Job")
-    name = 'speechlab-worker-job-{}-{}'.format(MODEL.lower().replace("_","-"), id_generator())
+    name = 'speechlab-worker-job-{}-{}'.format(
+        MODEL.lower().replace("_", "-"), id_generator())
     body.metadata = client.V1ObjectMeta(namespace=NAMESPACE, name=name)
     body.status = client.V1JobStatus()
     template = client.V1PodTemplate()
     template.template = client.V1PodTemplateSpec()
-
-    env_vars = { \
-    "AZURE_STORAGE_ACCOUNT" : AZURE_STORAGE_ACCOUNT, \
-    "AZURE_STORAGE_ACCESS_KEY": AZURE_STORAGE_ACCESS_KEY, \
-    "AZURE_CONTAINER": AZURE_CONTAINER, \
-    "MASTER": MASTER, \
-    "NAMESPACE": NAMESPACE, \
-    "RUN_FREQ": "ONCE", \
-    "MODEL_DIR": MODEL #important
+    pvc = client.V1PersistentVolumeClaimVolumeSource(
+        claim_name="models-azurefiles-claim"
+    )
+    volume = client.V1Volume(
+        name="models-azurefiles",
+        read_only=True,
+        secret_name=MODELS_FILESHARE_SECRET,
+        share_name=MODELS_SHARE_NAME,
+        persistent_volume_claim=pvc
+    )
+    env_vars = {
+        "AZURE_STORAGE_ACCOUNT": AZURE_STORAGE_ACCOUNT,
+        "AZURE_STORAGE_ACCESS_KEY": AZURE_STORAGE_ACCESS_KEY,
+        "AZURE_CONTAINER": AZURE_CONTAINER,
+        "MASTER": MASTER,
+        "NAMESPACE": NAMESPACE,
+        "RUN_FREQ": "ONCE",
+        "MODEL_DIR": MODEL,  # important
+        "MODELS_FILESHARE_SECRET": MODELS_FILESHARE_SECRET,
+        "MODELS_SHARE_NAME": MODELS_SHARE_NAME
     }
- 
+
     env_list = []
     if env_vars:
         for env_name, env_value in env_vars.items():
             env_list.append(client.V1EnvVar(name=env_name, value=env_value))
 
-    container = client.V1Container(name= '{}-c'.format(name),
-                                    image=IMAGE,
-                                    image_pull_policy="Always",
-                                    command=["/home/appuser/opt/tini", "--", "/home/appuser/opt/start_worker.sh"],                              
-                                    env=env_list,
-                                    security_context=client.V1SecurityContext(privileged=True, capabilities=client.V1Capabilities(add=["SYS_ADMIN"])),
-                                    resources=client.V1ResourceRequirements(limits={"memory":"5G","cpu": "1"}, requests={"memory":"5G", "cpu": "1"} )
+    container = client.V1Container(name='{}-c'.format(name),
+                                   image=IMAGE,
+                                   image_pull_policy="Always",
+                                   command=["/home/appuser/opt/tini", "--",
+                                            "/home/appuser/opt/start_worker.sh"],
+                                   env=env_list,
+                                   security_context=client.V1SecurityContext(
+                                       privileged=True, capabilities=client.V1Capabilities(add=["SYS_ADMIN"])),
+                                   resources=client.V1ResourceRequirements(
+                                       limits={"memory": "5G", "cpu": "1"}, requests={"memory": "5G", "cpu": "1"}),
+                                    volume_mounts=client.V1VolumeMount(
+                                        mount_path="/home/appuser/opt/models",
+                                        name="models-azurefiles",
+                                        read_only=True
                                     )
+                                   )
     template.template.spec = client.V1PodSpec(containers=[container],
-                                                image_pull_secrets= [{ "name":"azure-cr-secret"}],
-                                                restart_policy="OnFailure" #reason to use OnFailure https://github.com/kubernetes/kubernetes/issues/20255
-                                                )
-
-
+                                              image_pull_secrets=[
+                                                  {"name": "azure-cr-secret"}],
+                                              # reason to use OnFailure https://github.com/kubernetes/kubernetes/issues/20255
+                                              restart_policy="OnFailure",
+                                              volumes=[volume]
+                                              )
 
     # And finaly we can create our V1JobSpec!
-    body.spec = client.V1JobSpec(ttl_seconds_after_finished=100, template=template.template)
+    body.spec = client.V1JobSpec(
+        ttl_seconds_after_finished=100, template=template.template)
 
     try:
         api_response = api.create_namespaced_job(NAMESPACE, body)
-     
+
         #print("api_response="+ str(api_response))
         return True
     except ApiException as e:
