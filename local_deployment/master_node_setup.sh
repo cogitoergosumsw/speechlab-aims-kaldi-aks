c6cc025cd6a5247d9a6e93ec4ff20ab5c93ca9e4
Seng Wee <sengwee_new@hotmail.com>
2020-05-04 14:49:42 +0800

    add prometheus/grafana deploy

--- local_deployment/master_node_setup.sh
+++ local_deployment/master_node_setup.sh
@@ -155,6 +155,10 @@ sudo swapoff -a
 
 sudo chown -R $(id -u):$(id -g) $HOME/.kube/config
 
+sudo chmod +x deploy-prometheus-grafana.sh
+
+sudo ./deploy-prometheus-grafana.sh
+
 echo -e '\033[0;32m\nCongratulations, the Kubernetes cluster is set up now!\n\033[m'
 echo -e 'You can find the command for a worker node to join this Kubernetes cluster at \033[0;31m/local_deployment/kube_details.txt\033[m\n'
 
