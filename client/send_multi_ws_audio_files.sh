ac6ef6756bbb6c1be1e0c9d757f7d63a3a88bba0
Seng Wee <sengwee_new@hotmail.com>
2020-05-14 16:14:46 +0800

    reduce master CPU usage

--- client/send_multi_ws_audio_files.sh
+++ client/send_multi_ws_audio_files.sh
@@ -19,8 +19,8 @@ if [ "$1" == "--help" ]; then
 fi
 
 # change the IP address of the master service accordingly
-for i in {1..100}; do
-    python3 client/client_3_ssl.py -u ws://52.148.116.121/client/ws/speech \
+for i in {1..10}; do
+    python3 client/client_3_ssl.py -u ws://20.43.151.236/client/ws/speech \
         -r 32000 -t abc --model="SingaporeCS_0519NNET3" \
         docker/audio/long/episode-2-government-of-the-people-and-by-the-people.wav &
     sleep 10
